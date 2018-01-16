7206bd5ff92203748e594cdbd97caedf13d02805
munkhtsogt <munkhuu48@gmail.com>
2018-01-16 11:16:30 -0600

    bug fixed

--- lab1/Ex3.js
+++ lab1/Ex3.js
@@ -1,16 +1,8 @@
 function isWeekend(){
     const todayDate = new Date();
     const day = todayDate.getDay();
-	dic = {
-		0: "weekend",
-		6: "weekend",
-		1: "weekday",
-		2: "weekday",
-		3: "weekday",
-		4: "weekday",
-		5: "weekday",
-	}
-    return dic[day];
+	const i = Math.ceil((day % 6) / (day + Number.EPSILON));
+	return ["weekend", "weekday"][i];
 }
 
 console.log(isWeekend());
