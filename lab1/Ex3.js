57c3f68c3804cc2c47e1405a209a1c5b5211fbf5
munkhtsogt <munkhuu48@gmail.com>
2018-01-16 14:29:43 -0600

    lab1 is completed

--- lab1/Ex3.js
+++ lab1/Ex3.js
@@ -1,8 +1,8 @@
 function isWeekend(){
     const todayDate = new Date();
     const day = todayDate.getDay();
-	const i = Math.ceil((day % 6) / (day + Number.EPSILON));
-	return ["weekend", "weekday"][i];
+    const i = Math.ceil((day % 6) / (day + Number.EPSILON));
+    return ["weekend", "weekday"][i];
 }
 
 console.log(isWeekend());
