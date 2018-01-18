e7de3e2692b3c052358381f08bd1e7b4a5c98829
munkhtsogt <munkhuu48@gmail.com>
2018-01-18 16:46:07 -0600

    lab4 is completed

--- lab4/Ex2.js
+++ lab4/Ex2.js
@@ -5,14 +5,14 @@ let cpus = os.cpus().length, mem = os.totalmem/1e9;
 // Normal
 function checkSystem(){
     console.log('Checking your system...');
-    if(mem < 4.0) console.log('This app needs at least 4GB of RAM');
+    if(mem < 4) console.log('This app needs at least 4GB of RAM');
     if(cpus < 2) console.log('Processor is not supported');
     if(cpus >= 2 && mem >= 4) console.log('System is checked successfully.');
 }
 checkSystem();
 
 // Observable
-const source = Rx.Observable.create(ob => {
+Rx.Observable.create(ob => {
     ob.next('Checking your system...');
     if(mem < 4) ob.next('This app needs at least 4GB of RAM');
     if(cpus < 2) ob.next('Processor is not supported');
