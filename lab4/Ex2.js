504227b6e3f13362da53f6ddfa56c1d6a087de35
munkhtsogt <munkhuu48@gmail.com>
2018-01-18 20:46:58 -0600

    4GB precising

--- lab4/Ex2.js
+++ lab4/Ex2.js
@@ -6,16 +6,16 @@ let cpus = os.cpus().length, mem = os.totalmem()/1e9;
 // Normal
 function checkSystem(){
     console.log('Checking your system...');
-    if(mem < 4) console.log('This app needs at least 4GB of RAM');
+    if(mem < 4.096) console.log('This app needs at least 4GB of RAM');
     if(cpus < 2) console.log('Processor is not supported');
-    if(cpus >= 2 && mem >= 4) console.log('System is checked successfully.');
+    if(cpus >= 2 && mem >= 4.096) console.log('System is checked successfully.');
 }
 checkSystem();
 
 // Observable
 Rx.Observable.create(ob => {
     ob.next('Checking your system...');
-    if(mem < 4) ob.next('This app needs at least 4GB of RAM');
+    if(mem < 4.096) ob.next('This app needs at least 4GB of RAM');
     if(cpus < 2) ob.next('Processor is not supported');
-    if(cpus >= 2 && mem >= 4) ob.next('System is checked successfully.');
+    if(cpus >= 2 && mem >= 4.096) ob.next('System is checked successfully.');
 }).subscribe(v => console.log(v)).unsubscribe();
