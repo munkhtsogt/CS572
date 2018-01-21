e008d4e943fc9ad53a38a1d1b9163096e0acd1cc
munkhtsogt <munkhuu48@gmail.com>
2018-01-20 20:22:17 -0600

    Lab6 is completed

--- lab6/app.js
+++ lab6/app.js
@@ -1,6 +1,17 @@
 'use strict';
 const express = require('express');
+const validator = require('express-validator');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
+// PARSE application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// PARSE application/json
+app.use(bodyParser.json());
+// LOGGER
+app.use(morgan('combined'));
+// VALIDATOR
+app.use(validator());
 // ROUTE
 app.use('/api', require('./routes'));
 app.listen(3000, function(){
--- lab6/routes.js
+++ lab6/routes.js
@@ -1,26 +1,31 @@
 const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const validator = require('express-validator');
 const router = express.Router();
-const app = express();
-// PARSE application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// PARSE application/json
-app.use(bodyParser.json());
-// VALIDATOR
-app.use(validator());
-// LOGGER
-app.use(morgan('combined'));
+
 const grades = [{id: 1, name: 'Asaad Saad', course: 'CS572', grade: 95},
                 {id: 2, name: 'Munkhtsogt Tsogbadrakh', course: 'CS572', grade: 94},
                 {id: 3, name: 'Brad Pitt', course: 'CS572', grade: 93},
                 {id: 4, name: 'Tom Cruise', course: 'CS572', grade: 92}];
+// ACCEPT CROSS DOMAIN XHR REQUEST
+router.all('*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+// ALL LIST
+router.get('/grades', (req, res, next) => {
+    res.send(grades);
+});
 // CREATE
-router.post('/grades', (req, res) => {
+router.post('/grades', (req, res, next) => {
     req.checkBody("name", "Name is required").notEmpty();
     req.checkBody("course", "Course is required").notEmpty();
     req.checkBody("grade", "A valid grade is required").notEmpty().isNumeric();
+    var err = req.validationErrors();
+    if (err) {
+        res.send(err);
+        return;
+    }
     const grade = {
         id: grades[grades.length - 1].id + 1,
         name: req.body.name,
@@ -31,33 +36,38 @@ router.post('/grades', (req, res) => {
     res.send(grade);
 });
 // READ
-router.get('/grades/:id', (req, res) => {
+router.get('/grades/:id', (req, res, next) => {
     const i = grades.findIndex(grade => grade.id == req.params.id);
     if(i == -1) res.send({ 'message': 'grade not exists' });
-    res.send(grades[i]);
+    else res.send(grades[i]);
 });
 // UPDATE
-router.put('/grades/:id', (req, res) => {
-    req.checkBody("name", "Name is required").notEmpty();
-    req.checkBody("course", "Course is required").notEmpty();
-    req.checkBody("grade", "A valid grade is required").notEmpty().isNumeric();
+router.put('/grades/:id', (req, res, next) => {
     const i = grades.findIndex(grade => grade.id == req.params.id);
     if(i == -1) res.send({ 'message': 'grade not exists' });
-    grades[i].name = req.body.name;
-    grades[i].course = req.body.course;
-    grades[i].grade = req.body.grade;
-    res.send(grades[i]);
+    else {
+        req.checkBody("name", "Name is required").notEmpty();
+        req.checkBody("course", "Course is required").notEmpty();
+        req.checkBody("grade", "A valid grade is required").notEmpty().isNumeric();
+        var err = req.validationErrors();
+        if (err) {
+            res.send(err);
+            return;
+        }
+        grades[i].name = req.body.name;
+        grades[i].course = req.body.course;
+        grades[i].grade = req.body.grade;
+        res.send(grades[i]);
+    }
 });
 // DELETE
-router.delete('/grades/:id', (req, res) => {
+router.delete('/grades/:id', (req, res, next) => {
     const i = grades.findIndex(grade => grade.id == req.params.id);
     if(i == -1) res.send({ 'message': 'grade not exists' });
-    grades.splice(i, 1);
-    res.send({ 'message': 'successfully added' });
-});
-// ALL LIST
-router.get('/grades', (req, res) => {
-    res.send(grades);
+    else {
+        grades.splice(i, 1);
+        res.send({ 'message': 'successfully deleted' });
+    }
 });
 
 module.exports = router;
