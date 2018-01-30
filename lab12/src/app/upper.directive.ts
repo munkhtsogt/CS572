63687d85c6f1f1f664b7e3820fafd801148d6378
munkhtsogt <munkhuu48@gmail.com>
2018-01-30 15:43:45 -0600

    fixed intention

--- lab12/src/app/upper.directive.ts
+++ lab12/src/app/upper.directive.ts
@@ -5,10 +5,8 @@ import { Directive, ElementRef, Renderer2 } from '@angular/core';
 })
 export class UpperDirective {
 
-  constructor(private element: ElementRef, 
-              private renderer2: Renderer2) { 
-                
-                renderer2.setStyle(element.nativeElement, 'text-transform', 'uppercase');
-            }
+  constructor(private element: ElementRef, private renderer2: Renderer2) { 
+    renderer2.setStyle(element.nativeElement, 'text-transform', 'uppercase');
+  }
 
 }
