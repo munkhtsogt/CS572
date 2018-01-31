6534d7bb9ee31b120cf92ec4fe67eda6e2b4a849
munkhtsogt <munkhuu48@gmail.com>
2018-01-31 16:29:02 -0600

    unsubscribe is added

--- lab13/src/app/profile/profile.component.ts
+++ lab13/src/app/profile/profile.component.ts
@@ -1,24 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DbService } from '../db.service';
+import { Subscription } from 'rxjs/Rx';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnDestroy {
 
   student: any;
+  private subscription: Subscription;
   constructor(private route: ActivatedRoute, private dbService: DbService) { 
-    route.params.subscribe(params => {
+    this.subscription = route.params.subscribe(params => {
       let students = dbService.getData();
       let index = students.findIndex(s => s._id == params['id']);
       this.student = students[index];
     });
   }
 
-  ngOnInit() {
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
-
 }
